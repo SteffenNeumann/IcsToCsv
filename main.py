e27c492d6a3a10d13220212bc66b504e45767ae1
Mastercaution <neumann-steffen@web.de>
2024-06-06 07:57:09 +0200

    Update main.py
    sort option

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import csv
 from ics import Calendar
+
+
 def ics_to_csv(ics_file, csv_file):
     with open(ics_file, 'r') as ics:
         calendar = Calendar(ics.read())
+    events = list(calendar.events)
+    # Sortiere die Ereignisse nach start_datetime
+    events.sort(key=lambda event: event.begin, reverse=True)
     with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
         fieldnames = ['start_datetime', 'end_datetime', 'summary', 'description', 'location']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        
+
         writer.writeheader()
-        for event in calendar.events:
+        for event in events:
             writer.writerow({
                 'start_datetime': event.begin.to('local').format('YYYY-MM-DD HH:mm:ss'),
                 'end_datetime': event.end.to('local').format('YYYY-MM-DD HH:mm:ss'),
@@ -16,8 +21,10 @@ def ics_to_csv(ics_file, csv_file):
                 'description': event.description,
                 'location': event.location
             })
+
+
 if __name__ == "__main__":
-    ics_file = 'dein_dateiname.ics'
-    csv_file = 'output.csv'
+    ics_file = 'yourICSFile.ics'
+    csv_file = 'Output.csv'
     ics_to_csv(ics_file, csv_file)
-    print(f"Die Daten wurden erfolgreich von {ics_file} nach {csv_file} konvertiert.")
+    print(f"Die Daten wurden erfolgreich von {ics_file} nach {csv_file} konvertiert und sortiert.")
